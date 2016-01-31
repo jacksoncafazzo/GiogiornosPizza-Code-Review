1853c0a0f91c916923ccac8c480bdc24a060b2e3
Jackson Cafazzo <doctorjuno@gmail.com>
2016-01-31 03:16:00 -0800

    add tests to check user input for topping type and modify pizza

--- js/scripts.js
+++ js/scripts.js
@@ -148,7 +148,8 @@ $(document).ready(function () {
     console.log(pizza);
   });
 
-  $("input:checkbox.checked").click(function () {
+  $("input:checkbox").click(function (event) {
+    var toppingSection = this.name;
     if ($(this).hasClass("regular")) {
       var toppingType = "regular";
     } else if ($(this).hasClass("plus")) {
@@ -156,116 +157,148 @@ $(document).ready(function () {
     } else {
       console.log("whoa man");
     }
-    if ($(this).hasClass("sauce")) {
-      $(".currentSauce").append('<li>' + this.name + '</li>')
-      pizza.sauce.toppingType.append(this.name);
-    } else if ($(this).hasClass("cheese")) {
-      pizza.cheese.toppingType.append(this.name);
-    } else if ($(this).hasClass("veggies")) {
-      pizza.veggies.toppingType.append(this.name);
-    } else if ($(this).hasClass("meats")) {
-      pizza.meats.toppingType.append(this.name);
-    } else if ($(this).hasClass("seasoning")) {
-      pizza.seasoning.toppingType.append(this.name);
-    } else {
-      console.log("panic in the disco");
+
+    if ($(this).checked === true) {
+    } else if (toppingType === "plus")) {
+        if ($(this).hasClass("sauce")) {
+          $(".currentSauce").append('<li>' + this.name + '</li>');
+          pizza.sauce.plus.push(this.name);
+        } if ($(this).hasClass("cheese")) {
+          $(".currentCheese").push('<li>' + this.name + '</li>');
+          pizza.cheese.plus.push(this.name);
+        } if ($(this).hasClass("veggies")) {
+          $(".currentVeggies").push('<li>' + this.name + '</li>');
+          pizza.veggies.plus.push(this.name);
+        } if ($(this).hasClass("meats")) {
+          $(".currentMeats").push('<li>' + this.name + '</li>');
+          pizza.meats.plus.push(this.name);
+        } if ($(this).hasClass("seasoning")) {
+          $(".currentSeasoning").push('<li>' + this.name + '</li>');
+          pizza.seasoning.plus.push(this.name);
+        } else {
+          console.log("panic in the disco");
+        }
+      } else if ($(this).is("regular")) {
+        if ($(this).hasClass("sauce")) {
+          $(".currentSauce").push('<li>' + this.name + '</li>');
+          pizza.sauce.regular.push(this.name);
+        } else if ($(this).hasClass("cheese")) {
+          $(".currentCheese").push('<li>' + this.name + '</li>');
+          pizza.cheese.regular.push(this.name);
+        } else if ($(this).hasClass("veggies")) {
+          $(".currentVeggies").push('<li>' + this.name + '</li>');
+          pizza.veggies.regular.push(this.name);
+        } else if ($(this).hasClass("meats")) {
+          $(".currentMeats").push('<li>' + this.name + '</li>');
+          pizza.meats.regular.push(this.name);
+        } else if ($(this).hasClass("seasoning")) {
+          $(".currentSeasoning").push('<li>' + this.name + '</li>');
+          pizza.seasoning.regular.push(this.name);
+        } else {
+          console.log("panic in the other disco");
+        }
+      } else if ($(this).checked === false) {
+      for (var i = 0; i < Object.keys(pizza); i++) {
+        for (var i = 0; i < Object.keys(pizza); i++);
+        if (this.name === pizza.getOwnPropertyNames(pizza[i])) {
+          for (var j = 0; j < Object.keys(pizza[i]); j++) {
+            var toppingType = pizza[i];
+            if (this.name === pizza.toppingType[j]) {
+              pizza.toppingType[j] = [];
+            } else {
+              console.log("whoa nelly");
+            }
+          }
+        } else {
+          console.log("fail");
+        }
+      }
     }
 
-
   });
+    // then you make your pizza from the checked boxes and display it
 
-  $("input:checkbox.regular").click(function () {
-    pizza.toppingType.append(this.name);
+  // $("input:checkbox").click(function () {
+  //   debugger;
+  //
+  //
+  //
+  // });
 
-  });
+// populate list with checkboxs
 
-  $(".cheese").click(function () {
-    $(".currentCheese").append('<li>' + this.name + '</li>')
-  });
 
-  $(".veggies").click(function () {
-    $(".currentVeggies").append('<li>' + this.name + '</li>')
-  });
+  // $(".choosingSize").click(function () {
+  //   var pizza = {};
+  //   var sauces = [];
+  //   var cheeses = [];
+  //   var veggies = [];
+  //   var meats = [];
+  //   var seasonings = [];
+  //   var sauce = [];
+  //   var cheese = [];
+  //   var veggies = [];
+  //   var meats = [];
+  //   var seasoning = [];
+  //   var pizzaSize = [];
+  //   var plusSauce = [];
+  //   var plusCheese = [];
+  //   var plusVeggies = [];
+  //   var plusMeats = [];
+  //   var plusSeasoning = [];
+  //
+  //   $("input:checked.sauce").each(function () {
+  //     sauce.push($(this).attr("name"));
+  //   });
+  //   $("#plusSauce input:checked").each(function () {
+  //     plusSauce.push($(this).attr("name"));
+  //   });
+  //   sauce = new Sauce(sauce, plusSauce);
+  //   $("input:checked.cheese").each(function () {
+  //     cheese.push($(this).attr("name"));
+  //   });
+  //   $("#plusCheese input:checked").each(function () {
+  //     plusCheese.push($(this).attr("name"));
+  //   });
+  //   cheese = new Cheese(cheese, plusCheese);
+  //   debugger;
+  //   $("input:checked.veggies").each(function () {
+  //     veggies.push($(this).attr("name"));
+  //   });
+  //   $("#plusVeggies input:checked").each(function () {
+  //     plusVeggies.push($(this).attr("name"));
+  //   });
+  //   veggies = new Veggies(veggies, plusVeggies);
+  //   $("input:checked.meats").each(function () {
+  //     meats.push($(this).attr("name"));
+  //   });
+  //   $("#plusMeats input:checked").each(function () {
+  //     plusMeats.push($(this).attr("name"));
+  //   });
+  //   meats = new Meat(meats, plusMeats);
+  //   $("input:checked.seasoning").each(function () {
+  //     seasoning.push($(this).attr("name"));
+  //   });
+  //   $("#plusSeasoning input:checked").each(function () {
+  //     plusSeasoning.push($(this).attr("name"));
+  //   });
+  //   seasonings = new Seasoning(seasoning, plusSeasoning);
+  //
+  //   pizzaSize.push($(".choosingSize input:checked").attr("val"));
+  //   var pizza = new FlipPizza(sauces, cheeses, veggies, meats, seasonings, pizzaSize);
+  //   console.log(pizza);
+  //
+  //   $(".sauceTotal").text(Object.keys(pizza.sauce.plus.length * 0.5));
+  //
+  // });
 
-  $(".meats").click(function () {
-    $(".currentMeats").append('<li>' + this.name + '</li>')
-  });
-
-  $(".seasoning").click(function () {
-    $(".currentSeasoning").append('<li>' + this.name + '</li>')
-  });
-
-  $(".choosingSize").click(function () {
-    var pizza = {};
-    var sauces = [];
-    var cheeses = [];
-    var veggies = [];
-    var meats = [];
-    var seasonings = [];
-    var sauce = [];
-    var cheese = [];
-    var veggies = [];
-    var meats = [];
-    var seasoning = [];
-    var pizzaSize = [];
-    var plusSauce = [];
-    var plusCheese = [];
-    var plusVeggies = [];
-    var plusMeats = [];
-    var plusSeasoning = [];
-
-    $("input:checked.sauce").each(function () {
-      sauce.push($(this).attr("name"));
-    });
-    $("#plusSauce input:checked").each(function () {
-      plusSauce.push($(this).attr("name"));
-    });
-    sauce = new Sauce(sauce, plusSauce);
-    $("input:checked.cheese").each(function () {
-      cheese.push($(this).attr("name"));
-    });
-    $("#plusCheese input:checked").each(function () {
-      plusCheese.push($(this).attr("name"));
-    });
-    cheese = new Cheese(cheese, plusCheese);
-    debugger;
-    $("input:checked.veggies").each(function () {
-      veggies.push($(this).attr("name"));
-    });
-    $("#plusVeggies input:checked").each(function () {
-      plusVeggies.push($(this).attr("name"));
-    });
-    veggies = new Veggies(veggies, plusVeggies);
-    $("input:checked.meats").each(function () {
-      meats.push($(this).attr("name"));
-    });
-    $("#plusMeats input:checked").each(function () {
-      plusMeats.push($(this).attr("name"));
-    });
-    meats = new Meat(meats, plusMeats);
-    $("input:checked.seasoning").each(function () {
-      seasoning.push($(this).attr("name"));
-    });
-    $("#plusSeasoning input:checked").each(function () {
-      plusSeasoning.push($(this).attr("name"));
-    });
-    seasonings = new Seasoning(seasoning, plusSeasoning);
-
-    pizzaSize.push($(".choosingSize input:checked").attr("val"));
-    var pizza = new FlipPizza(sauces, cheeses, veggies, meats, seasonings, pizzaSize);
-    console.log(pizza);
-
-    $(".sauceTotal").text(Object.keys(pizza.sauce.plus.length * 0.5));
-
-  });
-
-  $("#newPizza").submit(function (event) {
-    var checkedBoxes = {}
-    $("input:checkbox.checked").each( function () {
-
-    });
-    console.log(checkedBoxes);
-  });
+  // $("#newPizza").submit(function (event) {
+  //   var checkedBoxes = {}
+  //   $("input:checkbox.checked").each( function () {
+  //
+  //   });
+  //   console.log(checkedBoxes);
+  // });
 
     // *******************************
     // $("." + tempRegular + "").each(function () {
